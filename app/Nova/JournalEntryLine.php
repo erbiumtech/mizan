ab3db5eea5785aa43c1af81ab007936ba9275e16
Ahmad Ishtiaq <meharahmii000@gmail.com>
2026-07-16 13:40:12 +0500

    Changes For Bank Details added in Employee

--- app/Nova/JournalEntryLine.php
+++ app/Nova/JournalEntryLine.php
@@ -43,6 +43,10 @@ class JournalEntryLine extends Resource
         ];
     }
 
+    // public static function authorizedToCreate(\Illuminate\Http\Request $request)
+    // {
+    //     return $request->user()->can('create', \App\Models\JournalEntry::class);
+    // }
     public static function authorizedToCreate(\Illuminate\Http\Request $request)
     {
         return $request->user()?->can('create', \App\Models\JournalEntry::class) ?? false;
