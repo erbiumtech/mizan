bc11b54b1e96f5d0baa31a4469a9bdce38fbbb0d
Muzafar Ali <mali@erbium.ch>
2026-09-10 11:31:22 +0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -120,8 +120,8 @@ return [
         'failover' => [
             'transport' => 'failover',
             'mailers' => [
-                'mailgun',
                 'sendgrid',
+                'mailgun',
             ],
         ],
 
